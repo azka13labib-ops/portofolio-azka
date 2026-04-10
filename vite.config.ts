afa3756710be3343ccc66ae58aa48e2b0e3009ca
azka13labib-ops <azka13labib@gmail.com>
2026-04-11 05:23:32 +0700

    fix: remove base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portofolio-azka/', // WAJIB DITAMBAHKAN AGAR GAMBAR & CSS TIDAK ERROR
+  // base: '/portofolio-azka/', <-- HAPUS ATAU BERI TANDA // SEPERTI INI
 })
